2eca42b98ffffe09f055d34c5299f978b8d0ffb8
Paul Fell <swellpf@gmail.com>
2019-07-12 19:12:57 -0400

    Updates to vacations controller

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,23 +1,55 @@
+require 'rack-flash'
+
 class VacationsController < ApplicationController
+  use Rack::Flash
 
   # GET: /vacations
   get "/vacations" do
-    erb :"/vacations/index.html"
+    if is_logged_in?
+      @vacations = Vacation.all
+      erb :"/vacations/index.html"
+    else
+      flash[:message] = "You must be logged in to create, edit or view vacation recommendations."
+      redirect to "/login"
+    end
   end
 
   # GET: /vacations/new
   get "/vacations/new" do
-    erb :"/vacations/new.html"
+    if is_logged_in?
+      erb :"/vacations/new.html"
+    else
+      flash[:message] = "You must be logged in to create vacation recommendations."
+      redirect to "/login"
+    end
   end
 
   # POST: /vacations
   post "/vacations" do
-    redirect "/vacations"
-  end
+    if is_logged_in?
+      # if conditional to make sure that all required fields are present
+          redirect to "/vacations/new"
+    else
+      @vacation = current_user.vacations.new(params)
+      if @vacation.save
+        redirect to "/vacations/#{vacation.id}"
+      else
+        flash[:message]= "Error creating vacation recommendation.  Please try again."
+        redirect to "/vacations/new"
+      end
+    else
+      flash[:message] = "You must be logged in to create or view vacation recommendations."
+      redirect to "/login"
+    end
 
   # GET: /vacations/5
   get "/vacations/:id" do
-    erb :"/vacations/show.html"
+    if is_logged_in?
+      @vacation = Vacation.find_by(params[:id])
+      erb :"/vacations/show.html"
+    else
+      redirect to "/login"
+    end
   end
 
   # GET: /vacations/5/edit
